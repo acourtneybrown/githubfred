fdf965e9616d9c04daa79c06c94b0c06eaf3481c
Adam Brown <acourtneybrown@gmail.com>
2025-07-02 12:38:24 -0700

    Add "default" Alfred item to open the notifications dashboard page

--- Workflow/notification.py
+++ Workflow/notification.py
@@ -16,6 +16,13 @@ from pyfred.model import (
 from pyfred.workflow import script_filter
 
 
+def _notifications_link_item(host=os.environ.get("github_host")) -> OutputItem:
+    return OutputItem(
+        title="View notifications dashboard on GitHub",
+        arg=["html", f"https://{host}/notifications"],
+    )
+
+
 @script_filter
 def main(
     script_path: Path, args_from_alfred: list[str], env: Optional[Environment]
@@ -28,22 +35,23 @@ def main(
     )
 
     if not response.ok:
-        return ScriptFilterOutput(items=[github.NO_RESULT])
+        return ScriptFilterOutput(items=[github.ERROR_RESULT])
 
     items = [
         OutputItem(
             title=n["subject"]["title"],
             subtitle=f"{n['repository']['full_name']}  ({github.human_date(n['updated_at'])} ago)",
-            arg=n["subject"]["url"],
+            arg=["api", n["subject"]["url"]],
         )
         for n in response.json()
     ]
 
     if items:
         return ScriptFilterOutput(
-            items=items, cache=CacheConfig(seconds=120, loosereload=True)
+            items=[*items, _notifications_link_item()],
+            cache=CacheConfig(seconds=120, loosereload=True),
         )
-    return ScriptFilterOutput(items=[github.NO_RESULT])
+    return ScriptFilterOutput(items=[github.NO_RESULT, _notifications_link_item()])
 
 
 if __name__ == "__main__":
